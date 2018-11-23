5f7a96c0bff64cb9f0b4b16838a743454cf8e859
gadi eichhorn <gadi.eichhorn@gmail.com>
2018-11-23 09:09:56 +0200

    remove eclipse framework

--- test/src/main/java/com/rds/demo/test/StatusFolowableConsumer.java
+++ test/src/main/java/com/rds/demo/test/StatusFolowableConsumer.java
@@ -1,18 +1,28 @@
 package com.rds.demo.test;
 
 import com.rds.demo.api.FlowableProvider;
+import io.reactivex.disposables.Disposable;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 
 @Component
 public class StatusFolowableConsumer {
+
+    Disposable disposable;
 
     @Reference
     private FlowableProvider<String> status;
 
     @Activate
     public void activate() {
-        status.subscribe(System.out::println);
+        disposable = status.subscribe(System.out::println);
     }
+
+    @Deactivate
+    public void deactivate() {
+        disposable.dispose();
+    }
+
 }
